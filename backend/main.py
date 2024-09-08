19c961b85362e8079e2499eb8baeb91e3380bc04
kllarena07 <baymax709@gmail.com>
2024-09-08 09:46:13 -0400

    feat: pass window href to flask backend

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app)
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+@app.route('/post', methods=['POST'])
+def receive_string():
+    received_data = request.get_json()
+    url = received_data.get('url')
+    print(url)
+    return 'Received string.'
 
 if __name__ == '__main__':
     app.run()
--- frontend/content_script.js
+++ frontend/content_script.js
@@ -1,2 +1,10 @@
 const windowHref = window.location.href;
 console.log(windowHref);
+
+fetch('http://127.0.0.1:5000/post', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({url: windowHref})
+});
