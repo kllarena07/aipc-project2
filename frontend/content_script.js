39add9f2acf4faa5042be53dfc585999fc3b1061
kllarena07 <baymax709@gmail.com>
2024-09-08 10:10:37 -0400

    feat: take screenshot of video and send to backend

--- frontend/content_script.js
+++ frontend/content_script.js
@@ -7,4 +7,34 @@ fetch('http://127.0.0.1:5000/post', {
         'Content-Type': 'application/json'
     },
     body: JSON.stringify({url: windowHref})
-}).then(response => console.log(response.json()));
+}).then(response => console.log(response));
+
+// add event listener to video element
+
+const getScreenshot = (video) => {
+    console.log("video was paused. Taking screenshot");
+    const canvas = document.createElement('canvas');
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+    const dataURL = canvas.toDataURL('image/png');
+    console.log(dataURL);
+
+    fetch('http://127.0.0.1:5000/upload', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({dataURL})
+    }).then(response => console.log(response));
+}
+
+let interval = setInterval(() => {
+    video = document.querySelector('video');
+    if (video) {
+        clearInterval(interval);
+        video.addEventListener('pause', () => getScreenshot(video));
+        console.log("Added event listener");
+    }
+}, 500);
