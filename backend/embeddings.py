2c812d2e5a1aee8761019039fa04262a9e0ca8dc
kllarena07 <baymax709@gmail.com>
2024-09-27 08:33:20 -0400

    refactor: model name var

--- backend/embeddings.py
+++ backend/embeddings.py
@@ -1,6 +1,8 @@
 from sentence_transformers import SentenceTransformer
 
+model_name = "mixedbread-ai/mxbai-embed-large-v1"
+
 def get_embeddings(docs, dimensions=512):
-    model = SentenceTransformer("mixedbread-ai/mxbai-embed-large-v1", truncate_dim=dimensions)
+    model = SentenceTransformer(model_name, truncate_dim=dimensions)
     embeddings = model.encode(docs)
     return embeddings
