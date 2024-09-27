9a430e9cc53ffbc0c8be5ea13677916bb1835357
kllarena07 <baymax709@gmail.com>
2024-09-27 08:43:10 -0400

    test: indexing and search on video load

--- backend/server.py
+++ backend/server.py
@@ -2,9 +2,21 @@ from flask import Flask, request
 from flask_cors import CORS
 from youtube_transcript_api import YouTubeTranscriptApi
 import base64
+from embeddings import get_embeddings
+from dotenv import load_dotenv
+from os import getenv
+from search import vector_search
+from pinecone_init import create_pinecone
+
+load_dotenv()
 
 app = Flask(__name__)
 cors = CORS(app)
+
+PINECONE_API_KEY = getenv('PINECONE_API_KEY')
+dimensions = 512
+index_name = "video-embeddings"
+pc = create_pinecone(api_key=PINECONE_API_KEY, dimensions=dimensions)
 
 @app.route('/post', methods=['POST'])
 def receive_string():
@@ -14,13 +26,39 @@ def receive_string():
     
     transcript = YouTubeTranscriptApi.get_transcript(video_id)
 
-    output=''
+    chunks = []
+
     for x in transcript:
         sentence = x['text']
-        output += f' {sentence}\n'
+        chunks.append(sentence)
 
-    print(output)
+    print(chunks)
 
+    embeddings = get_embeddings(chunks, dimensions)
+
+    upsert_data = []
+    for i, (text, embedding) in enumerate(zip(chunks, embeddings)):
+        upsert_data.append((
+            f"id_{i}",
+            embedding.tolist(),
+            {"text": text}
+        ))
+
+    index = pc.Index(index_name)
+    index.upsert(
+        vectors=upsert_data,
+        namespace=url
+    )
+
+    query = "What is OpenVINO?"
+    search_results = vector_search(pc=pc, index_name=index_name, namespace=url, dimensions=dimensions, query=query)
+    print("search results:", search_results)
+
+    for result in search_results:
+        print(f'{result['score']} : {result['metadata']['text']}')
+        print('----------')
+
+    print("Sending response back to client.")
     return 'Received string.'
 
 @app.route('/upload', methods=['POST'])
